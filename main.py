374146964002b94c3c38c0957a7f85548e37b773
yuki hara <hara.0509@icloud.com>
2021-01-12 17:28:09 +0900

    例外処理追加

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 import random
 
-my_hand = int(input('グー(0)チョキ(1)パー(2)>'))
-rhand = random.randint(0,2)
+my_hand = input('グー(0)チョキ(1)パー(2)>')
+r_hand = random.randint(0,2)
 
-if (my_hand == 0 or my_hand == 1 or my_hand == 2):
-  if rhand == 0:
-    if rhand == my_hand:
-      print('あいこ')
-    elif rhand == (my_hand -1):
-      print('負け')
-    elif rhand == my_hand -2 :
-      print('勝ち')
+try:
+  my_hand = int(my_hand)
+  if (my_hand >= 0 and my_hand <= 2):
+    if r_hand == 0:
+      if r_hand == my_hand:
+        print('あいこ')
+      elif r_hand == (my_hand -1):
+        print('負け')
+      elif r_hand == my_hand -2 :
+        print('勝ち')
 
-  elif rhand == 1:
-    if rhand == my_hand+1:
-      print('かち')
-    elif rhand == my_hand:
-      print('あいこ')
-    elif rhand == my_hand -1 :
-      print('負け')
+    elif r_hand == 1:
+      if r_hand == my_hand+1:
+        print('かち')
+      elif r_hand == my_hand:
+        print('あいこ')
+      elif r_hand == my_hand -1 :
+        print('負け')
 
-  elif rhand == 2:
-    if rhand == my_hand+2:
-      print('負け')
-    elif rhand == my_hand+1:
-      print('かち')
-    elif rhand == my_hand:
-      print('あいこ')
-
-else:
+    elif r_hand == 2:
+      if r_hand == my_hand+2:
+        print('負け')
+      elif r_hand == my_hand+1:
+        print('かち')
+      elif r_hand == my_hand:
+        print('あいこ')
+  else:
+    print('正しく入力してください')
+except:
   print('正しく入力してください')
