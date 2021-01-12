24aab800759f33f24eb5ee609174bb26d5d4785a
yuki hara <hara.0509@icloud.com>
2021-01-12 18:00:32 +0900

    繰り返し処理追加

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import random
 
-my_hand = input('グー(0)チョキ(1)パー(2)>')
-r_hand = random.randint(0,2)
+choice = input('グー(0)チョキ(1)パー(2)>')
+my_hands = ['グー','チョキ','パー']
+judge = [
+  ['あいこ','負け','かち'],
+  ['かち','あいこ','負け'],
+  ['負け','かち','あいこ']
+]
+result = ''
 
 try:
-  my_hand = int(my_hand)
-  if (my_hand >= 0 and my_hand <= 2):
-    if r_hand == 0:
-      if r_hand == my_hand:
-        print('あいこ')
-      elif r_hand == (my_hand -1):
-        print('負け')
-      elif r_hand == my_hand -2 :
-        print('勝ち')
+  choice = int(choice)
+  if (choice >= 0 and choice <= 2):
+    print(my_hands[choice])
 
-    elif r_hand == 1:
-      if r_hand == my_hand+1:
-        print('かち')
-      elif r_hand == my_hand:
-        print('あいこ')
-      elif r_hand == my_hand -1 :
-        print('負け')
+    while result == 'あいこ' or result == '':
+      r_hand = random.randint(0,2)
+      # 勝敗判定
+      if r_hand == 0:
+        if r_hand == choice:
+          result = judge[0][0]
+        elif r_hand == choice - 1:
+          result = judge[0][1]
+        elif r_hand == choice - 2 :
+          result = judge[0][2]
 
-    elif r_hand == 2:
-      if r_hand == my_hand+2:
-        print('負け')
-      elif r_hand == my_hand+1:
-        print('かち')
-      elif r_hand == my_hand:
-        print('あいこ')
+      elif r_hand == 1:
+        if r_hand == choice + 1:
+          result = judge[1][0]
+        elif r_hand == choice:
+          result = judge[1][1]
+        elif r_hand == choice - 1 :
+          result = judge[1][2]
+
+      elif r_hand == 2:
+        if r_hand == choice + 2:
+          result = judge[2][0]
+        elif r_hand == choice + 1:
+          result = judge[2][1]
+        elif r_hand == choice:
+          result = judge[2][2]
+      print(result)
   else:
     print('正しく入力してください')
 except:
